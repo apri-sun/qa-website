8487e6c35365c72e56df0b07ad5ea3548aded82d
bekyiu <651731457@qq.com>
2019-03-29 17:59:46 +0800

    关注列表 在静态页面中用绝对路径引入资源 不然在请求的时候静态资源请求路径会加上奇怪的东西 ????

--- src/main/java/bekyiu/web/controller/FollowController.java
+++ src/main/java/bekyiu/web/controller/FollowController.java
@@ -12,11 +12,12 @@ import bekyiu.util.JsonUtil;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -113,7 +114,12 @@ public class FollowController
         return getBackInfo(questionId, EntityType.ENTITY_QUESTION, ret);
     }
 
-
+    //跳转到粉丝列表
+    @RequestMapping("/user/{userId}/followers")
+    public String followerList(Model model, @PathVariable Long userId)
+    {
+        return "followList";
+    }
 
 
     //关注人/问题 后应该返回的信息
