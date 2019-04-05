7ecc4b93120ba546bdce07c4f7ff6499c2d9e51a
bekyiu <651731457@qq.com>
2019-04-05 20:30:49 +0800

    SpringBoot是可以帮你自动做很多事，但有时候带来不是方便而是错误，比如在启动集成了solr的springboot项目时，它自动创建了一个HttpSolrClient，而这个会和你自己在项目中创建的HttpSolrClient发生冲突，所以导致错误 解决方法：通过在启动类禁用Solr的自动配置即可

--- src/main/java/bekyiu/QaApplication.java
+++ src/main/java/bekyiu/QaApplication.java
@@ -3,9 +3,10 @@ package bekyiu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SolrAutoConfiguration.class)
 @MapperScan("bekyiu.mapper")
 @EnableTransactionManagement
 public class QaApplication
